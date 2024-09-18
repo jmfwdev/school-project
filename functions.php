ec4f8976811d13ec1ad0da1daad3d70af016f9b6
JM Hore <kira@MacBook-Air.local>
2024-09-18 13:18:16 -0700

    fixed footer

--- functions.php
+++ functions.php
@@ -157,7 +157,7 @@ add_action('wp_enqueue_scripts', 'school_theme_scripts');
 
 function fwd_custom_font()
 {
-	wp_register_style(' my-google-font ', ' https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap ', false);
+	wp_register_style('my-google-font', 'https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap', false);
 	wp_enqueue_style('my-google-font');
 }
 
